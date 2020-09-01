5db82740113d59702022a2f9c0f1e46311259987
Simon Freyburger <simonfreyburger@yahoo.fr>
2020-09-01 16:07:06 +0200

    avec la pièce jointe c'est mieux...

---
+++ autoaugment/config.py
@@ -0,0 +1,34 @@
+from autoaugment.retrieve_data import get_dummy_sample
+from autoaugment.compute_all import main_compute
+from autoaugment.learning_curve import plot_result
+from autoaugment.transforms.identity import identity, identity_ml
+from autoaugment.transforms.masking import mask_along_axis_random
+from braindecode.datasets.transform_classes import TransformSignal, TransformFFT
+import mne
+
+
+params_masking = {"mask_value": 0.0,
+                  "mask_param": 10,
+                  "axis": 2}
+
+dataset_args = {"transform_type": "raw (no transforms)",
+                "transform_list": [[TransformSignal(identity)]]}
+
+shallow_args = {"model_type": "ShallowFBCSPNet",
+                "batch_size": 64,
+                "seed": None,
+                "lr": 0.00625,
+                "weight_decay": 0,
+                "n_epochs": 3,
+                "n_cross_val": 3,
+                "try": "bar",
+                "train_split": None,
+                }
+
+saving_params = {"result_dict_name": "dummy_dict",
+                 "folder": "/storage/store/work/sfreybur/result_folder/"}
+
+hf_args = {"model_type": "RandomForest",
+           "n_cross_val": 3}
+
+sample_size_list = [0.01, 0.1, 0.25, 0.5, 1]
