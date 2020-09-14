b106a34b888b8ca6794cc0d55d3305f37d65f86e
Simon Freyburger <simonfreyburger@yahoo.fr>
2020-09-14 15:52:37 +0200

    add new files

---
+++ autoaugment/construct_transforms.py
@@ -0,0 +1,41 @@
+from braindecode.datasets.transform_classes import TransformSignal, \
+    TransformFFT
+
+from .transforms.signal_merging import merge_two_signals
+from .transforms.delaying_signal import delay_signal
+from .transforms.noise_addition import add_noise_to_signal
+from .transforms.masking import mask_along_axis_random
+from .transforms.identity import identity
+from .transforms.em_decomposition import merge_two_imfs
+
+
+def construct_transforms(dataset_args, transforms_args):
+    transform_list = []
+    for transform_name in dataset_args["transform_list"]:
+        transform = []
+        for operation in transform:
+            if operation == "merge_two_signals":
+                transform.append(TransformSignal(
+                    merge_two_signals, transforms_args))
+            elif operation == "identity":
+                transform.append(TransformSignal(identity, transforms_args))
+            elif operation == "delay_signal":
+                transform.append(TransformSignal(
+                    delay_signal, transforms_args))
+            elif operation == "add_noise_to_signal":
+                transform.append(TransformSignal(
+                    add_noise_to_signal, transforms_args))
+            elif operation == "merge_two_imfs":
+                transform.append(TransformSignal(
+                    merge_two_imfs, transforms_args))
+            elif operation == "mask_along_time":
+                transform.append(TransformFFT(
+                    mask_along_axis_random, transforms_args))
+            elif operation == "mask_along_frequency":
+                transform.append(TransformFFT(
+                    mask_along_axis_random, transforms_args))
+            else:
+                raise ValueError(
+                    "This transform is currently not implemented")
+        transform_list.append(transform)
+    return(transform_list)
---
+++ autoaugment/transforms/em_decomposition.py
@@ -0,0 +1,30 @@
+import random
+import numpy as np
+from PyEMD import EMD
+
+
+def merge_two_imfs(datum, params):
+    signal = datum.X
+    y = datum.y
+    train_sample = params["train_sample"]
+    label_index_dict = params["label_index_dict"]
+    other_signal_index = random.choice(label_index_dict[y])
+    other_signal = train_sample[other_signal_index][1]
+    imfs_signal = get_same_shaped_imfs(signal)
+    other_imfs_signal = get_same_shaped_imfs(other_signal)
+    final_signal = np.zeros(imfs_signal.shape[1])
+    for i in range(12):
+        if random.random() <= params["magnitude"]:
+            final_signal += other_imfs_signal[i, :]
+        else:
+            final_signal += imfs_signal[i, :]
+    datum.X = final_signal
+    return datum
+
+
+def get_same_shaped_imfs(signal):
+    emd = EMD(max_imfs=12)
+    imfs_signal = emd(signal)
+    new_imfs = np.zeros((imfs_signal.shape[1], 12))
+    new_imfs[:imfs_signal.shape[0], :] = imfs_signal
+    return new_imfs
